4ee6455eced271a638222f50d780845cc0384760
Simon Janda <janda.simon@icloud.com>
2020-03-13 11:23:33 +0100

    Prepared the ground for form error handling

--- auth.js
+++ auth.js
@@ -7,28 +7,32 @@ const auth = {
 	open: ['/login', '/signup', '/api/login', '/api/signup'],
 
 	login: (req, res) => {
-		db.oneOrNone('SELECT * FROM participant WHERE email = $1', req.body.email)
-		.then(data => {
-			if (data.password) {
-				bcrypt.compare(req.body.password, data.password, (err, result) => {
-				if (result) {
-					req.session.user = { id: data.id, name: data.name, email: data.email }
-					res.redirect('/')
+		if (req.body.email.trim() == '' || req.body.password == '') {
+			res.redirect('/login')
+		} else {
+			db.oneOrNone('SELECT * FROM participant WHERE email = $1', req.body.email)
+			.then(data => {
+				if (data.password) {
+					bcrypt.compare(req.body.password, data.password, (err, result) => {
+					if (result) {
+						req.session.user = { id: data.id, name: data.name, email: data.email }
+						res.redirect('/')
+					} else {
+						res.redirect('/login')
+						console.log('failed to sign in')
+					}
+					})
 				} else {
+					req.error.exist = 'test'
 					res.redirect('/login')
-					console.log('failed to sign in')
 				}
-				})
-			} else {
-				req.error.exist = 'test'
-				res.redirect('/login')
-			}
-			
-		})
-		.catch(err => {
-			console.log(err)
-			res.redirect(403, '/login')
-		})
+				
+			})
+			.catch(err => {
+				console.log(err)
+				res.redirect(403, '/login')
+			})
+		}
 	},
 
 	logout: (req, res) => {
